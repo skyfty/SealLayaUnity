dec6ca9cb9219f92d0f50fe9f8ad311187a55c12
skyfty <38198234+skyfty@users.noreply.github.com>
2020-09-13 13:33:07 +0800

    init

---
+++ Assets/Script/Create.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour
+{
+    public const float maxPositionZ = 1.2f;
+    public const float maxRotation = 70f;
+    public float tempPositionZ;
+    public float tempRotation;
+    public float tempScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(this.transform.childCount);
+        Initial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Initial() {
+        for (int i=0;i<this.transform.childCount;i++) {
+            //tempPositionZ = Random.Range(-Vector3.Distance(this.transform.localPosition,Vector3.zero), 90f);
+            tempPositionZ = Random.Range(-maxPositionZ, maxPositionZ);
+            tempRotation = Random.Range(-maxRotation, maxRotation);
+            tempScale = 1 / Mathf.Cos(tempRotation / 180 * 3.1415f);
+            if (Random.Range(0, 2) == 0) {
+                this.transform.GetChild(i).transform.localRotation = Quaternion.Euler(new Vector3(tempRotation, 0, 0));
+                this.transform.GetChild(i).transform.localScale = new Vector3(1f, tempScale, 1f);
+            } else {
+                this.transform.GetChild(i).transform.localRotation = Quaternion.Euler(new Vector3(0, tempRotation, 0));
+                this.transform.GetChild(i).transform.localScale = new Vector3(tempScale, 1f, 1f);
+            }
+            this.transform.GetChild(i).transform.localPosition = new Vector3(this.transform.GetChild(i).transform.localPosition.x, this.transform.GetChild(i).transform.localPosition.y, tempPositionZ);
+            //this.child[i].transform.localPosition = new Vector3
+            //Debug.Log(this.transform.childCount);
+        }
+    }
+    
+}
---
+++ Assets/Script/Sphere_demo_004.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Sphere_demo : MonoBehaviour
+{
+    public GameObject go;
+    public float rotationX;
+    public float rotationY;
+    public float scartX;
+    public float scartY;
+    public float a, b, c, d;
+    void Start()
+    {
+        b = Random.Range(1, 3);
+
+        if (b == 1)
+        {
+            rotationX = Random.Range(-70, 70);
+            transform.rotation = Quaternion.Euler(new Vector3(rotationX, 0, 0));
+            scartY = 2 / Mathf.Cos(rotationX / 180 * 3.1415f);
+            go.transform.localScale = new Vector3(2, scartY, 0.1f);
+
+            a = Random.Range(-4, 4);
+            transform.position = new Vector3(transform.position.x, transform.position.y, a);
+        }
+
+        if (b == 2)
+        {
+            rotationY = Random.Range(-70, 70);
+            transform.rotation = Quaternion.Euler(new Vector3(0, rotationY, 0));
+            scartX = 2 / Mathf.Cos(rotationY / 180 * 3.1415f);
+            go.transform.localScale = new Vector3(scartX, 2, 0.1f);
+
+            a = Random.Range(-4, 4);
+            transform.position = new Vector3(transform.position.x, transform.position.y, a);
+        }
+    }
+
+    
+}
